75c100c78c5ca304cad6eda9ddfe2eeef2765e51
Abhi <abhinav.panwarr@gmail.com>
2024-09-13 00:22:38 +0530

    append server

--- http-server/server.js
+++ http-server/server.js
@@ -1,8 +1,20 @@
 const http = require("http");
+const fs = require("fs")
 
 const myServer = http.createServer((req, res) => {
-    console.log(req);
-    res.end("hellow frm server");
+    const log = `${Date.now()}: ${req.url} New req received...\n`;
+    fs.appendFile("log.txt", log, (err,data) => {
+        switch (req.url){
+            case "/":
+                res.end("homepage");
+                break;
+            case "/about":
+                res.end("Abhi");
+                break;
+            default:
+                res.end("404 not found");
+        }
+    })
 });
 
 myServer.listen(8000, () => console.log("server started!!!"));
