6a0873bac7f5b676c01ab066fc19988e5c746ebd
Abhi <abhinav.panwarr@gmail.com>
2024-09-13 00:47:51 +0530

    url parsed

--- http-server/server.js
+++ http-server/server.js
@@ -1,8 +1,14 @@
 const http = require("http");
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
     const log = `${Date.now()}: ${req.url} New req received...\n`;
+    const myUrl = url.parse(req.url);
+    // this url.parse seperates the req.url into its components
+
+    console.log(myUrl);
+
     fs.appendFile("log.txt", log, (err,data) => {
         switch (req.url){
             case "/":
